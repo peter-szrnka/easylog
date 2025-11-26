112db7769e9dd69255abe810bb43a362125b78df
Péter Szrnka <szrnka.peter@gmail.com>
2025-11-26 07:37:41 +0100

    EL-0 Mvn cmd fine tuned

--- client/easylog-client/build.gradle.kts
+++ client/easylog-client/build.gradle.kts
@@ -47,9 +47,11 @@ dependencies {
 
 }
 
+val mvnCmd = if (System.getProperty("os.name").startsWith("Windows")) "mvnw.cmd" else "./mvnw"
+
 tasks.register<Exec>("buildServerCommon") {
     workingDir = file("../server")
-    commandLine("mvn", "clean", "install", "-DskipTests", "-Ponly-common")
+    commandLine(mvnCmd, "clean", "install", "-DskipTests", "-Ponly-common")
 }
 
 
