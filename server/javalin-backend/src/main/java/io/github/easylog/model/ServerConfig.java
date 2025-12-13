996e6a6c119e504fe3ed6b3c84c3e3568742503f
Péter Szrnka <szrnka.peter@gmail.com>
2025-12-13 11:42:25 +0100

    EL-6 Missing properties added

--- server/javalin-backend/src/main/java/io/github/easylog/model/ServerConfig.java
+++ server/javalin-backend/src/main/java/io/github/easylog/model/ServerConfig.java
@@ -2,6 +2,13 @@ package io.github.easylog.model;
 
 /**
  * @author Peter Szrnka
+ *
+ * @param port Server port
+ * @param sslEnabled SSL enabled or disabled (by default it's enabled)
+ * @param serviceName NSD Service name
+ * @param serviceType NSD service type
+ * @param serverDbFile Server db file, e.g.: easylog-local.db
+ * @param env Environment
  */
 public record ServerConfig(
         int port,
